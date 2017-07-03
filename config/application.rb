9ef48808c5ed010ed9ef095a0e109475601689e4
Alexandre Bouchard <Alexandre Bouchard>
2017-07-02 22:14:41 -0500

    new branc push

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 require_relative 'boot'
 
-require 'rails/all'
-
+#require 'rails/all'
+require 'sinatra'
+require 'mongoid'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
