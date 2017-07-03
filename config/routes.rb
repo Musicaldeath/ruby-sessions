8c29478ba0a986d7fac07e3648d61cea312e0913
Alexandre Bouchard <Alexandre Bouchard>
2017-07-02 23:14:35 -0500

    setting up api

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   #SESSIONS
-  post    'session',            to: 'session#validate'
   get     'session',            to: 'session#create'
-  get     'session/:id',        to: 'session#regenerate'
+  post    'session/:id',        to: 'session#validate'
+  patch   'session/:id',        to: 'session#regenerate'
   delete  'session/:id',        to: 'session#destroy'
 
 end
