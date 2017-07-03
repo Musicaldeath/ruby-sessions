1751ca603b646d60eb4c49c2c0972784920a4ed6
Alexandre Bouchard <Alexandre Bouchard>
2017-07-02 22:33:26 -0500

    w

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.2'
 gem 'sinatra'
 gem 'mongoid'
+
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 # Use Puma as the app server
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ require 'mongoid'
 Bundler.require(*Rails.groups)
 
 module RubySessions
+
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
